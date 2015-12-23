d147a921fce9577f9154b06fb146e203d7a171fc
David García <davidgf1987@gmail.com>
2015-12-23 14:44:32 +0100

    Basic feature tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'search/index'
+
+  get '/search' => 'search#search', :as => :search
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'search#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
